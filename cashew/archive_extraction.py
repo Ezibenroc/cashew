5fe9031c63f9e3c3d6f0e22e79db37f5a02985c1
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2019-06-14 14:17:19 +0200

    First commit

---
+++ cashew/archive_extraction.py
@@ -0,0 +1,39 @@
+import io
+import zipfile
+import pandas
+import os
+import datetime
+import yaml
+import re
+
+
+def read_csv(archive_name, csv_name, columns=None):
+    archive = zipfile.ZipFile(archive_name)
+    df = pandas.read_csv(io.BytesIO(archive.read(csv_name)), names=columns)
+    df.columns = df.columns.str.strip()
+    return df
+
+
+def read_yaml(archive_name, yaml_name):
+    archive = zipfile.ZipFile(archive_name)
+    return yaml.load(archive.read(yaml_name), Loader=yaml.SafeLoader)
+
+
+def read_file(archive_name, csv_name, columns=None):
+    df = read_csv(archive_name, csv_name, columns)
+    info = read_yaml(archive_name, 'info.yaml')
+    nodes = [key for key in info if key.endswith('grid5000.fr')]
+    assert len(nodes) == 1
+    node = nodes[0]
+    node = node[:node.index('.')]
+    node = int(node[node.index('-')+1:])
+    df['node'] = node
+    df['cluster'] = info['cluster']
+    df['jobid'] = info['jobid']
+    df['cpu'] = 2*df['node'] + df['core'] % 2 - 2
+    oarstat = read_yaml(archive_name, 'oarstat.yaml')
+    df['start_date'] = str(datetime.datetime.fromtimestamp(oarstat['startTime']).date())
+    df['index'] = -1
+    for core in df['core'].unique():
+        df.loc[df['core'] == core, 'index'] = range(len(df[df['core'] == core]))
+    return df
---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import sys
+from setuptools import setup
+import subprocess
+
+VERSION = '0.0.0'
+
+
+class CommandError(Exception):
+    pass
+
+
+def run(args):
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    if proc.returncode != 0:
+        raise CommandError('Error with the command %s.\n' % ' '.join(args))
+    return stdout.decode('ascii').strip()
+
+
+def git_version():
+    return run(['git', 'rev-parse', 'HEAD'])
+
+
+def git_tag():
+    return run(['git', 'describe', '--always', '--dirty'])
+
+
+def write_version(filename, version_dict):
+    with open(filename, 'w') as f:
+        for version_name in version_dict:
+            f.write('%s = "%s"\n' % (version_name, version_dict[version_name]))
+
+
+if __name__ == '__main__':
+    try:
+        write_version('cashew/version.py', {
+                '__version__': VERSION,
+                '__git_version__': git_version(),
+            })
+    except CommandError as e:
+        if sys.argv[0] != '-c':
+            sys.exit(e)
+    setup(name='cashew',
+          version=VERSION,
+          description='Utility functions to work with archives produced by the peanut engine',
+          author='Tom Cornebize',
+          author_email='tom.cornebize@gmail.com',
+          packages=['cashew'],
+          install_requires=[
+              'pandas',
+              'pyyaml',
+          ],
+          url='https://github.com/Ezibenroc/cashew',
+          license='MIT',
+          classifiers=[
+              'License :: OSI Approved :: MIT License',
+              'Intended Audience :: Developers',
+              'Operating System :: POSIX :: Linux',
+              'Operating System :: MacOS :: MacOS X',
+              'Programming Language :: Python :: 3.6',
+          ],
+          )
