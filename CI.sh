f81305d755a20c9b1ad17a68a8682aa626ee4135
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-04-22 17:14:15 +0200

    Add the script CI.sh

---
+++ CI.sh
@@ -0,0 +1,20 @@
+TOKEN_PATH="GITLAB_TOKEN"
+REPO_URL="https://gitlab.in2p3.fr/tom.cornebize/g5k_data.git"
+
+token=$(cat $TOKEN_PATH)
+if [ $? -ne 0 ] ; then
+    echo "Missing gitlab token, exiting"
+    exit 1
+fi
+
+echo "###  PROCESSING"
+cd /tmp
+base_url=$(echo $REPO_URL | awk -F '//' '{ printf $2; }')
+remote_url="https://oauth2:$token@$base_url"
+git clone $REPO_URL repository --depth 1
+cd repository
+git remote set-branches origin '*'  # https://stackoverflow.com/a/27393574
+git fetch -v
+process_archive | grep "No new archive" && exit 0
+git push $remote_url
+for branch in $(cat processed_branches) ; do git push $remote_url --delete $branch ; done
