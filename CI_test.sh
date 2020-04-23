9ed85264bc7440afc91cac0eb385a81e9fd325c5
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-04-23 18:52:42 +0200

    Fix

--- CI_test.sh
+++ CI_test.sh
@@ -12,8 +12,8 @@ cd /tmp
 base_url=$(echo $REPO_URL | awk -F '//' '{ printf $2; }')
 remote_url="https://oauth2:$token@$base_url"
 GIT_LFS_SKIP_SMUDGE=1 git clone $REPO_URL repository --depth 1
+cd repository
 git config user.email "CI_test@$(hostname)"
 git config user.name "gitlab-CI-test"
-cd repository
 cashew test --output notebooks all && git add notebooks && git commit -m "[AUTOMATIC COMMIT] Generating test notebooks"
 git push $remote_url
