021a5a33e786ee4f63be5956363f77bbe311ea4f
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-12-20 19:40:15 +0100

    Fix

--- reprocess_all.sh
+++ reprocess_all.sh
@@ -19,15 +19,19 @@ git remote add origin $new_remote
 
 for year in {2019,2020}; do
     for month in {01..12} ; do
-        for f in $(find data -name "*${year}-${month}-*") ; do
+        archives=false
+        for f in $(find data -name "*${year}-${month}-*" | sort) ; do
             echoerr "Processing file $f"
             cashew extract $f performance data.db --compression zlib --compression_lvl 9 --format table
             cashew extract $f monitoring data_monitoring.db --compression zlib --compression_lvl 9 --format table || echoerr "    Failed to extract monitoring data"
+            archives=true
         done
-        cashew stats data.db stats.csv
-        cashew stats data_monitoring.db stats_monitoring.csv
-        git add .
-        git commit -m """[AUTOMATIC COMMIT] Reprocessing all the archives for ${year}-${month}"""
-        git push --set-upstream origin master || git push || git push || git push
+        if [ $archives = true ] ; then
+            cashew stats data.db stats.csv
+            cashew stats data_monitoring.db stats_monitoring.csv
+            git add .
+            git commit -m """[AUTOMATIC COMMIT] Reprocessing all the archives for ${year}-${month}"""
+            git push --set-upstream origin master || git push || git push || git push
+        fi
     done
 done
