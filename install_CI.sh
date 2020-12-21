de94b4c314607594ee566bf7137c491be1f2de4f
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-12-21 20:00:51 +0100

    Version 0.1.0

--- install_CI.sh
+++ install_CI.sh
@@ -1,7 +1,7 @@
 sudo-g5k apt update
 sudo-g5k apt remove -y python3-pandas # the debian package is very outdated...
 sudo-g5k apt install --no-install-recommends -y git-lfs libhdf5-dev libhdf5-serial-dev
-sudo-g5k pip3 install https://github.com/Ezibenroc/cashew/releases/download/0.0.18/cashew-0.0.18-py3-none-any.whl
+sudo-g5k pip3 install https://github.com/Ezibenroc/cashew/releases/download/0.1.0/cashew-0.1.0-py3-none-any.whl
 cashew --git-version
 sudo-g5k wget https://raw.githubusercontent.com/Ezibenroc/cashew/master/process_archives.sh -O /bin/process_archive
 sudo-g5k chmod +x /bin/process_archive
