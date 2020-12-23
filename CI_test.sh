be10348aab9308977446213fba86376bf404e233
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-12-23 10:19:00 +0100

    Fix the script

--- CI_test.sh
+++ CI_test.sh
@@ -2,14 +2,14 @@ SSH_URL="git@github.com:Ezibenroc/g5k_test.git"
 
 echo "###  PROCESSING"
 cd /tmp
-mkdir -p notebooks && cd notebooks
-cashew test --output . all && cd .. || exit 1
+rm -rf notebooks
+cashew test --output notebooks all || exit 1
 git clone $SSH_URL repository
 cd repository
 # Let's keep a single commit in this repository, we do not need history, so better to save some space
 rm -rf .git
 git init
-mv ../*.html .
+mv ../notebooks/*.html .
 git add .
 git config user.email "CI_test@$(hostname)"
 git config user.name "CI-test"
