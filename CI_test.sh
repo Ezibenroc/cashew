121e42059aa0c43fb0f0e689f43fb328f9b037a5
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2020-12-23 09:23:55 +0100

    Switch to github for the website
    IN2P3's gitlab is now too unreliable for that.

--- CI_test.sh
+++ CI_test.sh
@@ -1,23 +1,20 @@
-TOKEN_PATH="GITLAB_TOKEN"
-REPO_URL="https://gitlab.in2p3.fr/cornebize/g5k_test.git"
-SSH_URL="git@gitlab.in2p3.fr:cornebize/g5k_test.git"
-
-token=$(cat $TOKEN_PATH)
-if [ $? -ne 0 ] ; then
-    echo "Missing gitlab token, exiting"
-    exit 1
-fi
+SSH_URL="git@github.com:Ezibenroc/g5k_test.git"
 
 echo "###  PROCESSING"
 cd /tmp
-base_url=$(echo $REPO_URL | awk -F '//' '{ printf $2; }')
-remote_url="https://oauth2:$token@$base_url"
-test -d repository || GIT_LFS_SKIP_SMUDGE=1 git clone $SSH_URL repository --depth 1
+mkdir -p notebooks && cd notebooks
+cashew test --output . all && cd .. || exit 1
+git clone $SSH_URL repository
 cd repository
+# Let's keep a single commit in this repository, we do not need history, so better to save some space
+rm -rf .git
+git init
+mv ../*.html .
+git add .
 git config user.email "CI_test@$(hostname)"
-git config user.name "gitlab-CI-test"
-cd notebooks && cashew test --output . all && cd .. || exit 1
-mkdir -p public && mv notebooks/*.html public || exit 1
-git add notebooks public && git commit -m "[AUTOMATIC COMMIT] Generating test notebooks"
-git push $SSH_URL
+git config user.name "CI-test"
+git commit -am "[AUTOMATIC COMMIT] Regenerate notebooks"
+git remote add origin $SSH_URL
+git push --set-upstream origin master --force
+
 echo "DONE: $0"
